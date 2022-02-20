2ca049594bf578c190a2634aec8ae080a2db672b
icarusw-code <77170611+icarusw-code@users.noreply.github.com>
2022-02-20 23:04:13 +0900

    data jpa

---
+++ data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,11 @@
+package study.datajpa.repository;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MemberJpaRepositoryTest {
+
+
+}
